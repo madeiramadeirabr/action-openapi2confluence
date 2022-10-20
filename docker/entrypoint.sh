569375ce615f79c3c3ae0b79de72d19136498668
lucasres <lucasresone@gmail.com>
2022-10-20 18:33:16 -0300

    add " nas flags

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -9,4 +9,4 @@ echo $6
 echo $7
 echo $8
 
-openapi2confluence -p $1 -id $2 -s $3 -a $4 -t $5 -lid $6 -mid $7 -env $8 >> $GITHUB_STEP_SUMMARY
+openapi2confluence -p "$1" -id "$2" -s "$3" -a "$4" -t "$5" -lid "$6" -mid "$7" -env "$8" >> $GITHUB_STEP_SUMMARY
